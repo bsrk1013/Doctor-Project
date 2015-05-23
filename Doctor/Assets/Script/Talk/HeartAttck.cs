6dca0619b3ce22dc7bb0c7d11f254933c6546192
Baeck <bsrk1013@naver.com>
2015-05-23 14:22:10 +0900

    2015. 05. 23 * 채팅창 터치시 대화 넘김 구현

---
+++ Doctor/Assets/Script/Talk/HeartAttck.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HeartAttck : Talk {
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        RunTime();
+
+        DefaultTalk();
+    }
+}
---
+++ Doctor/Assets/Script/Talk/Talk.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Talk : MonoBehaviour {
+
+    public string[] TalkList = new string[20];
+    public float LimitTime;
+    public int LimitTalkNum;
+
+    protected UILabel Label;
+    protected float CurrentTime;
+    protected int CurrentTalkNum;
+
+    protected void Init()
+    {
+        CurrentTalkNum = 0;
+        CurrentTime = 0;
+        Label = GetComponent<UILabel>();
+        Label.fontSize = 40;
+    }
+
+    // Update가 시작하자마자 실행
+    protected void RunTime()
+    {
+        CurrentTime += RealTime.deltaTime;
+    }
+
+    protected void DefaultTalk()
+    {
+        Label.text = TalkList[CurrentTalkNum];
+
+        if( LimitTime <= CurrentTime )
+        {
+            NextTalk();
+        }
+    }
+
+    protected void NextTalk()
+    {
+        if (LimitTalkNum > CurrentTalkNum)
+        {
+            ++CurrentTalkNum;
+        }
+        else
+        {
+            CurrentTalkNum = 0;
+        }
+        CurrentTime = 0;
+    }
+
+    public void TouchWindow()
+    {
+        NextTalk();
+    }
+}
